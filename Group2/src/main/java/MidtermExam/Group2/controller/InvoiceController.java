79cb4e53221a6f00bb3c1debcc9af7d631cd3303
Alif T <alift929@gmail.com>
2024-07-25 11:12:04 +0700

    feat: add excel export feature

--- Group2/src/main/java/MidtermExam/Group2/controller/InvoiceController.java
+++ Group2/src/main/java/MidtermExam/Group2/controller/InvoiceController.java
@@ -4,16 +4,21 @@ import MidtermExam.Group2.criteria.InvoiceSearchCriteria;
 import MidtermExam.Group2.dto.InvoiceDTO;
 import MidtermExam.Group2.dto.InvoiceDetailDTO;
 import MidtermExam.Group2.dto.InvoiceListDTO;
+import MidtermExam.Group2.service.ExportService;
 import MidtermExam.Group2.service.InvoiceService;
 import jakarta.validation.Valid;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,15 +28,29 @@ import java.util.UUID;
 @RequestMapping("/api/v1/invoices")
 public class InvoiceController {
     private final InvoiceService invoiceService;
+    private final ExportService exportService;
 
     @Autowired
-    public InvoiceController(InvoiceService invoiceService) {
+    public InvoiceController(InvoiceService invoiceService, ExportService exportService) {
         this.invoiceService = invoiceService;
+        this.exportService = exportService;
     }
 
     @GetMapping
     public ResponseEntity<Page<InvoiceListDTO>> getAllInvoices(Pageable pageable, InvoiceSearchCriteria criteria) {
         return ResponseEntity.ok(invoiceService.getAllInvoices(pageable, criteria));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getInvoiceDetail(@PathVariable("id") UUID invoiceId) {
+        try {
+            InvoiceDetailDTO invoiceDetail = invoiceService.getInvoiceDetail(invoiceId);
+            return new ResponseEntity<>(invoiceDetail, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            Map<String, String> response = new HashMap<>();
+            response.put("errors", e.getMessage());
+            return ResponseEntity.badRequest().body(response);
+        }
     }
 
     @PostMapping
@@ -59,15 +78,23 @@ public class InvoiceController {
         }
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<?> getInvoiceDetail(@PathVariable("id") UUID invoiceId) {
+    @GetMapping("/excel")
+    public ResponseEntity<?> exportInvoicesToExcel(@RequestParam(required = false) UUID customerId,
+                                                   @RequestParam(required = false) Integer month,
+                                                   @RequestParam(required = false) Integer year) {
         try {
-            InvoiceDetailDTO invoiceDetail = invoiceService.getInvoiceDetail(invoiceId);
-            return new ResponseEntity<>(invoiceDetail, HttpStatus.OK);
-        } catch (RuntimeException e) {
-            Map<String, String> response = new HashMap<>();
-            response.put("errors", e.getMessage());
-            return ResponseEntity.badRequest().body(response);
+            ByteArrayInputStream excelFile = exportService.exportInvoicesToExcel(customerId, month, year);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Disposition", "attachment; filename=invoices.xlsx");
+
+            return ResponseEntity
+                    .ok()
+                    .headers(headers)
+                    .contentType(MediaType.parseMediaType("application/vnd.ms-excel"))
+                    .body(excelFile.readAllBytes());
+        } catch (IOException e) {
+            return ResponseEntity.status(500).body("Failed to export invoices to Excel");
         }
     }
 }
