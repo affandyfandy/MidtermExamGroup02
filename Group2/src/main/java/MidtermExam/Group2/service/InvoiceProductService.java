65c9cf904715f3341af8ebc383d9bd8728b3a097
Alif T <alift929@gmail.com>
2024-07-25 15:28:03 +0700

    feat: add delete invoice products

--- Group2/src/main/java/MidtermExam/Group2/service/InvoiceProductService.java
+++ Group2/src/main/java/MidtermExam/Group2/service/InvoiceProductService.java
@@ -12,4 +12,5 @@ public interface InvoiceProductService {
     Page<InvoiceProductDTO> getAllInvoiceProducts(Pageable pageable);
     InvoiceProductDTO addInvoiceProduct(InvoiceProductDTO invoiceProductDTO);
     InvoiceProductDTO editInvoiceProduct(InvoiceProductDTO invoiceProductDTO, UUID invoiceId, UUID productId);
+    void deleteInvoiceProduct(UUID invoiceId, UUID productId);
 }
